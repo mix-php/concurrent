9afc2c3eefd2d9e4094fd1d80b9b512d11898f61
Jian Liu <onanying@126.com>
2019-03-07 18:00:09 +0800

    更新

--- src/CoroutinePool/Worker.php
+++ src/CoroutinePool/Worker.php
@@ -3,7 +3,7 @@
 namespace Mix\Concurrent\CoroutinePool;
 
 use Mix\Core\Bean\AbstractObject;
-use Swoole\Coroutine\Channel;
+use Mix\Core\Coroutine\Channel;
 use Mix\Core\Coroutine;
 
 /**
@@ -16,19 +16,19 @@ class Worker extends AbstractObject
 
     /**
      * 工作池
-     * @var \Swoole\Coroutine\Channel
+     * @var \Mix\Core\Coroutine\Channel
      */
     public $workerPool;
 
     /**
      * 任务通道
-     * @var \Swoole\Coroutine\Channel
+     * @var \Mix\Core\Coroutine\Channel
      */
     public $jobChannel;
 
     /**
      * 退出
-     * @var \Swoole\Coroutine\Channel
+     * @var \Mix\Core\Coroutine\Channel
      */
     protected $_quit;
 
