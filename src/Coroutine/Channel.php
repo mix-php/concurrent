73d0e834fc101ca7066cad147842c1d3598be4d7
Jian Liu <onanying@126.com>
2019-06-19 19:23:11 +0800

    Coroutine移动到concurrent库

---
+++ src/Coroutine/Channel.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Mix\Concurrent\Coroutine;
+
+/**
+ * Class Channel
+ * @package Mix\Core\Coroutine
+ * @author liu,jian <coder.keda@gmail.com>
+ *
+ * @method bool push($data)
+ * @method mixed pop()
+ * @method bool isEmpty()
+ * @method bool isFull()
+ * @method array stats()
+ * @method int length()
+ * @method close()
+ */
+class Channel extends \Swoole\Coroutine\Channel
+{
+
+    /**
+     * Channel constructor.
+     * 为了代码补全才加的，@method无法设置构造方法
+     * @param int $capacity
+     */
+    public function __construct(int $capacity = 1)
+    {
+        parent::__construct($capacity);
+    }
+
+}
